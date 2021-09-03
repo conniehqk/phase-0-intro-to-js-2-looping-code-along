0cabd1898b2c0333a5684f9dbd2c4af04a5476d6
conniekang <hqk1121@gmail.com>
2021-09-03 13:26:55 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function writeCards(name, event) {
+    let out = []
+    for (let i = 0; i < name.length; i++) {
+        out.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return out
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
